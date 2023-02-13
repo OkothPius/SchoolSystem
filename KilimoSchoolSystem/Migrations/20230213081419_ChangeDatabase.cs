f7480afe64fec175b3143e05a50a17741b800b04
Okoth Pius <orukopius8@gmail.com>
2023-02-13 12:36:53 +0300

    feat: change Database and migrate

---
+++ KilimoSchoolSystem/Migrations/20230213081419_ChangeDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KilimoSchoolSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
